00cc2060f97362dcd3f221255d7e505385087e62
alien4rmspace <damian4lif@yahoo.com>
2025-12-24 17:43:42 -0800

    Model is trained and results are plotted onto a visual chart.

--- main.py
+++ main.py
@@ -18,3 +18,21 @@ model = nn.Sequential(nn.Linear(1,1))
 # Define the loss function and optimizer
 loss_function = nn.MSELoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+# Train the model
+for epoch in range(500):
+    # Reset the optimizer's gradients'
+    optimizer.zero_grad()
+    # Make predictions
+    outputs = model(distances)
+    # Calculate the loss / difference
+    loss = loss_function(outputs, times)
+    # Calculate adjustments
+    loss.backward()
+    # Update the model's parameter
+    optimizer.step()
+    # Print loss every 50 epochs
+    if (epoch + 1) % 50  == 0:
+        print(f"Epoch {epoch + 1}: Loss = {loss.item()}")
+
+helper_utils.plot_results(model, distances, times)
