9b5231b2f60f0fafc6c15d8f61b1e1e4f2b3e46e
alien4rmspace <damian4lif@yahoo.com>
2026-01-02 22:13:20 -0800

    Trained first non-liner neural network.

--- non_linear_model.py
+++ non_linear_model.py
@@ -19,7 +19,6 @@ times = torch.tensor([
     [76.34], [76.38], [78.34], [80.07], [81.86], [84.45], [83.98], [86.55], [88.33], [86.83],
     [89.24], [88.11], [88.16], [91.77], [92.27], [92.13], [90.73], [90.39], [92.98]], dtype=torch.float32)
 
-#helper_utils.plot_data(distances,times)
 
 """
 Normalize the data.
@@ -37,4 +36,45 @@ times_std = times.std()
 distances_normalized = (distances - distances_mean) / distances_std
 times_normalized = (times - times_mean) / times_std
 
-helper_utils.plot_data(distances_normalized, times_normalized, normalize=True)
+"""
+Create our model
+"""
+
+# Seed for torch
+torch.manual_seed(27)
+
+model = nn.Sequential(
+    nn.Linear(1, 3),
+    nn.ReLU(),
+    nn.Linear(3, 1)
+)
+
+"""
+Train the model
+"""
+
+# Define the loss function and optimizer
+loss_function = nn.MSELoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+# Training loop
+for epoch in range(3000):
+    optimizer.zero_grad()  # Reset every loop. We do not want it to start from where it left off.
+    outputs = model(distances_normalized) # Make predictions (forward pass)
+    loss = loss_function(outputs, times_normalized) # Calculate the loss
+    loss.backward() # Calculate adjustments (backward pass)
+    optimizer.step() # Update model's parameters
+
+    # Create live plots every 50 epochs (loops)
+    if (epoch + 1) % 50 == 0:
+        helper_utils.plot_training_progress(
+            epoch = epoch,
+            loss = loss,
+            model = model,
+            distances_norm = distances_normalized,
+            times_norm =  times_normalized
+        )
+print("\nTraining Completed.")
+print(f"\nFinal Loss: {loss.item()}")
+
+helper_utils.plot_final_fit(model, distances, times, distances_normalized, times_std, times_mean)
