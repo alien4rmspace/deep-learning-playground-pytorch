52708350a994903391b9ed0401faa20e1a70256c
alien4rmspace <damian4lif@yahoo.com>
2026-01-02 22:02:45 -0800

    Normalized data sets.

--- non_linear_model.py
+++ non_linear_model.py
@@ -19,4 +19,22 @@ times = torch.tensor([
     [76.34], [76.38], [78.34], [80.07], [81.86], [84.45], [83.98], [86.55], [88.33], [86.83],
     [89.24], [88.11], [88.16], [91.77], [92.27], [92.13], [90.73], [90.39], [92.98]], dtype=torch.float32)
 
-helper_utils.plot_data(distances,times)
+#helper_utils.plot_data(distances,times)
+
+"""
+Normalize the data.
+"""
+
+# Calculate mean and standard deviation for 'distance' tensor.
+distances_mean = distances.mean()
+distances_std = distances.std()
+
+# Calculate mean and standard deviation  for 'times' tensor.
+times_mean = times.mean()
+times_std = times.std()
+
+# Apply standardization to the distances & times.
+distances_normalized = (distances - distances_mean) / distances_std
+times_normalized = (times - times_mean) / times_std
+
+helper_utils.plot_data(distances_normalized, times_normalized, normalize=True)
